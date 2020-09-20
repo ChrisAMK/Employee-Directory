98720bc0c102064f111293ac72650e3b450552b7
Chris <chriskl@live.com>
2020-09-20 22:14:50 +0800

    Updates

---
+++ precache-manifest.a31982c49d3231b90e7ce2c0c6a839af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a993ae4a67f393537530c3c2b0e098af",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "c35f36de79b8116e8a1f",
+    "url": "/Employee-Directory/static/css/main.997bdfec.chunk.css"
+  },
+  {
+    "revision": "628f013f998371920597",
+    "url": "/Employee-Directory/static/js/2.02c2c5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.02c2c5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35f36de79b8116e8a1f",
+    "url": "/Employee-Directory/static/js/main.696f1629.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  }
+]);
